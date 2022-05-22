77fe39006128375f2b65aa7dcf2342afb64199b1
unknown <leminhyen2@gmail.com>
2022-05-22 13:54:31 +0700

    update replaceVocab

--- replaceVocabThenSaveModel.py
+++ replaceVocabThenSaveModel.py
@@ -1,20 +1,25 @@
 def replaceVocabThenSaveModel(updatedModelFileName, originalVocab, newVocab, score=0):
     import sentencepiece.sentencepiece_model_pb2 as model
 
-    m = model.ModelProto()
-    m.ParseFromString(open("spm.ja.nopretok.model", "rb").read())
+    from checkIfVocabInModel import checkIfVocabInModel
+    
+    if( checkIfVocabInModel(newVocab, updatedModelFileName) == True):
+        return "new vocab is already in the model, no need to do anything"
+    else:
+        m = model.ModelProto()
+        m.ParseFromString(open("spm.ja.nopretok.model", "rb").read())
 
-    for i, vocab in enumerate(m.pieces):
-        vocabContent = vocab.piece
-        if (vocabContent == originalVocab):
-            vocab.score = score
-            vocab.piece = newVocab
+        for i, vocab in enumerate(m.pieces):
+            vocabContent = vocab.piece
+            if (vocabContent == originalVocab):
+                vocab.score = score
+                vocab.piece = newVocab
 
-    with open(updatedModelFileName, 'wb') as f:
-        f.write(m.SerializeToString())
+        with open(updatedModelFileName, 'wb') as f:
+            f.write(m.SerializeToString())
 
 
-# replaceVocabThenSaveModel("new.model", "🐬", "<MASK>", 0)
+replaceVocabThenSaveModel("new.model", "🐬", "<MASK>", 0)
 
 # from writeListOfSPMvocabsToFile import writeListOfSPMvocabsToFile
 # writeListOfSPMvocabsToFile("new.model", "englishVocab.txt")
